84385c30a0d1613a5f5e53d1637703119d98435e
Brent Dimmig <brentdimmig@gmail.com>
2017-02-27 21:45:10 -0500

    improve usage by implementing Hotword struct and AddHotword on detector

--- example/main.go
+++ example/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"os"
 	"fmt"
+	"strings"
 	"io/ioutil"
 
 	"github.com/brentnd/go-snowboy"
@@ -13,14 +14,34 @@ func main() {
 		fmt.Printf("usage: %s <resource> <keyword.umdl> <audio file>\n", os.Args[0])
 		return
 	}
-	d := snowboy.NewSnowboyDetector(os.Args[1], os.Args[2], "0.5", 1.0)
+	d := snowboy.Detector{
+		ResourceFilename: os.Args[1],
+		AudioGain: 1.0,
+	}
 	defer d.Close()
+
+	// Parse keyword from .umdl file path.
+	// If keyword is known (not command line), just use
+	// snowboy.Keyword("i know it already")
+	k := os.Args[2]
+	k = strings.TrimRight(k, ".umdl")
+	kParts := strings.Split(k, "/")
+
+	d.AddHotword(snowboy.Hotword{
+		Model: os.Args[2],
+		Sensitivity: 0.5,
+		Keyword: snowboy.Keyword(kParts[len(kParts) - 1]),
+	})
 
 	wav, err := ioutil.ReadFile(os.Args[3])
 	if err != nil {
 		panic(err)
 	}
 
-	res := d.RunDetection(wav)
-	fmt.Printf("detected %d\n", res)
+	res, err := d.RunDetection(wav)
+	if res != nil {
+		fmt.Printf("detected keyword %v, error %v\n", *res, err)
+	} else {
+		fmt.Printf("detect no keyword, error %v\n", err)
+	}
 }
--- snowboy.go
+++ snowboy.go
@@ -5,22 +5,58 @@ import (
 	"errors"
 
 	"github.com/Kitt-AI/snowboy/swig/Go"
+	"strconv"
 )
+
+type Keyword string
+const (
+	KeywordSilence = Keyword("silence")
+	KeywordAlexa   = Keyword("alexa")
+	KeywordSnowboy = Keyword("snowboy")
+)
+
+type Hotword struct {
+	Model       string
+	Sensitivity float32
+	Keyword     Keyword
+}
 
 type Detector struct {
 	raw snowboydetect.SnowboyDetect
+	keywords          []Keyword
+	sensitivity       string
+	modelStr          string
+	initialized       bool
+	ResourceFilename  string
+	AudioGain         float32
 }
 
-func NewSnowboyDetector(resource, keywords, sensitivity string, audioGain float32) Detector {
-	d := Detector{}
-	d.raw = snowboydetect.NewSnowboyDetect(resource, keywords)
-	d.raw.SetSensitivity("0.5")
-	d.raw.SetAudioGain(audioGain)
-	return d
+func (d *Detector) AddHotword(h Hotword) error {
+	if d.initialized {
+		return errors.New("detector already initialized")
+	}
+	if len(d.keywords) > 0 {
+		d.modelStr += ","
+		d.sensitivity += ","
+	}
+	d.modelStr += h.Model
+	d.sensitivity += strconv.FormatFloat(float64(h.Sensitivity), 'f', 2, 64)
+	d.keywords = append(d.keywords, h.Keyword)
+	return nil
+}
+
+func (d *Detector) initialize() {
+	if d.initialized {
+		return
+	}
+	d.raw = snowboydetect.NewSnowboyDetect(d.ResourceFilename, d.modelStr)
+	d.raw.SetSensitivity(d.sensitivity)
+	d.raw.SetAudioGain(d.AudioGain)
+	d.initialized = true
 }
 
 func (d *Detector) Close() error {
-	if d.raw != nil {
+	if d.initialized {
 		snowboydetect.DeleteSnowboyDetect(d.raw)
 		return nil
 	} else {
@@ -28,7 +64,22 @@ func (d *Detector) Close() error {
 	}
 }
 
-func (d *Detector) RunDetection(data []byte) int {
+func (d *Detector) RunDetection(data []byte) (*Keyword, error) {
+	d.initialize()
+	if len(data) == 0 {
+		return nil, nil
+	}
 	ptr := snowboydetect.SwigcptrInt16_t(unsafe.Pointer(&data[0]))
-	return d.raw.RunDetection(ptr, len(data) / 2 /* len of int16 */)
+	res := d.raw.RunDetection(ptr, len(data) / 2 /* len of int16 */)
+	if res == -2 {
+		k := KeywordSilence
+		return &k, nil
+	} else if res == -1 {
+		// TODO: extract real error if possible
+		return nil, errors.New("snowboy error")
+	} else if res == 0 {
+		return nil, nil
+	} else {
+		return &d.keywords[res - 1], nil
+	}
 }
