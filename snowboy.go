bebe1d4aaf167a6cac325090faabf1ad706760d0
Brent Dimmig <brentdimmig@gmail.com>
2017-03-01 09:04:40 -0500

    re-order functions to match documentation order

--- snowboy.go
+++ snowboy.go
@@ -1,62 +1,16 @@
 package snowboy
 
 import (
-	"unsafe"
 	"errors"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"strconv"
+	"strings"
+	"unsafe"
 
 	"github.com/Kitt-AI/snowboy/swig/Go"
-	"io"
-	"strconv"
-	"fmt"
-	"io/ioutil"
-	"strings"
 )
-
-// A Handler is used to handle when keywords are detected
-//
-// Detected will be call with the keyword string
-type Handler interface {
-	Detected(string)
-}
-
-type handlerKeyword struct {
-	Handler
-	keyword string
-}
-
-func (h handlerKeyword) call() {
-	h.Handler.Detected(h.keyword)
-}
-
-type handlerFunc func(string)
-
-func (f handlerFunc) Detected(keyword string) {
-	f(keyword)
-}
-
-// A Hotword represents a model filename and sensitivity for a snowboy detectable word
-//
-// Model is the filename for the .umdl file
-// Sensitivity is the sensitivity of this specific hotword
-// Name is what will be used in calls to Handler.Detected(string)
-type Hotword struct {
-	Model       string
-	Sensitivity float32
-	Name        string
-}
-
-// Creates a hotword from model and sensitivity only, parsing
-// the hotward name from the model filename
-func NewHotword(model string, sensitivity float32) Hotword {
-	h := Hotword{
-		Model: model,
-		Sensitivity: sensitivity,
-	}
-	name := strings.TrimRight(model, ".umdl")
-	nameParts := strings.Split(name, "/")
-	h.Name = nameParts[len(nameParts) - 1]
-	return h
-}
 
 // Detector is holds the context and base impl for snowboy audio detection
 type Detector struct {
@@ -78,13 +32,6 @@ func NewDetector(resourceFile string) Detector {
 	}
 }
 
-func (d *Detector) initialize() {
-	d.raw = snowboydetect.NewSnowboyDetect(d.ResourceFile, d.modelStr)
-	d.raw.SetSensitivity(d.sensitivityStr)
-	d.raw.SetAudioGain(d.AudioGain)
-	d.initialized = true
-}
-
 // Close handles cleanup required by snowboy library
 //
 // Clients must call Close on detectors after doing any detection
@@ -96,32 +43,6 @@ func (d *Detector) Close() error {
 		return nil
 	} else {
 		return errors.New("snowboy not initialize")
-	}
-}
-
-func (d *Detector) runDetection(data []byte) int {
-	if len(data) == 0 {
-		return 0
-	}
-	ptr := snowboydetect.SwigcptrInt16_t(unsafe.Pointer(&data[0]))
-	return d.raw.RunDetection(ptr, len(data) / 2 /* len of int16 */)
-}
-
-func (d *Detector) route(result int) {
-	if result == -2 {
-		// TODO: silence handler
-		fmt.Println("silence")
-	} else if result == -1 {
-		fmt.Println("error in snowboy")
-	} else if result == 0 {
-		fmt.Println("nothing detected")
-	} else if result > 0 {
-		handlerKeyword, ok := d.handlers[result]
-		if ok {
-			handlerKeyword.call()
-		} else {
-			fmt.Println("no handler for result", result)
-		}
 	}
 }
 
@@ -158,4 +79,85 @@ func (d *Detector) ReadAndDetect(data io.Reader) (err error) {
 	}
 	d.route(d.runDetection(bytes))
 	return
+}
+
+func (d *Detector) initialize() {
+	d.raw = snowboydetect.NewSnowboyDetect(d.ResourceFile, d.modelStr)
+	d.raw.SetSensitivity(d.sensitivityStr)
+	d.raw.SetAudioGain(d.AudioGain)
+	d.initialized = true
+}
+
+func (d *Detector) route(result int) {
+	if result == -2 {
+		// TODO: silence handler
+		fmt.Println("silence")
+	} else if result == -1 {
+		fmt.Println("error in snowboy")
+	} else if result == 0 {
+		fmt.Println("nothing detected")
+	} else if result > 0 {
+		handlerKeyword, ok := d.handlers[result]
+		if ok {
+			handlerKeyword.call()
+		} else {
+			fmt.Println("no handler for result", result)
+		}
+	}
+}
+
+func (d *Detector) runDetection(data []byte) int {
+	if len(data) == 0 {
+		return 0
+	}
+	ptr := snowboydetect.SwigcptrInt16_t(unsafe.Pointer(&data[0]))
+	return d.raw.RunDetection(ptr, len(data) / 2 /* len of int16 */)
+}
+
+// A Handler is used to handle when keywords are detected
+//
+// Detected will be call with the keyword string
+type Handler interface {
+	Detected(string)
+}
+
+type handlerKeyword struct {
+	Handler
+	keyword string
+}
+
+func (h handlerKeyword) call() {
+	h.Handler.Detected(h.keyword)
+}
+
+type handlerFunc func(string)
+
+func (f handlerFunc) Detected(keyword string) {
+	f(keyword)
+}
+
+// A Hotword represents a model filename and sensitivity for a snowboy detectable word
+//
+// Model is the filename for the .umdl file
+//
+// Sensitivity is the sensitivity of this specific hotword
+//
+// Name is what will be used in calls to Handler.Detected(string)
+type Hotword struct {
+	Model       string
+	Sensitivity float32
+	Name        string
+}
+
+// Creates a hotword from model and sensitivity only, parsing
+// the hotward name from the model filename
+func NewHotword(model string, sensitivity float32) Hotword {
+	h := Hotword{
+		Model: model,
+		Sensitivity: sensitivity,
+	}
+	name := strings.TrimRight(model, ".umdl")
+	nameParts := strings.Split(name, "/")
+	h.Name = nameParts[len(nameParts) - 1]
+	return h
 }
