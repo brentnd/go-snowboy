7b363926f6504ca417eb3274bbd6e0065cc2e011
Brent Dimmig <brentdimmig@gmail.com>
2017-03-06 15:51:44 -0500

    add test for handling silence functions

--- snowboy_test.go
+++ snowboy_test.go
@@ -1,6 +1,8 @@
 package snowboy
 
 import (
+	"os"
+	"time"
 	"testing"
 	"github.com/stretchr/testify/assert"
 )
@@ -12,6 +14,8 @@ const (
 
 func TestNewDetector(t *testing.T) {
 	d := NewDetector(resourceFile)
+	defer d.Close()
+
 	assert.Equal(t, resourceFile, d.ResourceFile, "NewDetect sets ResourceFile")
 	assert.Equal(t, float32(1.0), d.AudioGain, "Gain should default to 1.0")
 }
@@ -26,4 +30,26 @@ func TestDetector_AudioFormat(t *testing.T) {
 	assert.Equal(t, 16000, a)
 	assert.Equal(t, 1, b)
 	assert.Equal(t, 16, c)
+}
+
+func TestDetector_HandleSilenceFunc(t *testing.T) {
+	d := NewDetector(resourceFile)
+	defer d.Close()
+
+	// Not under test but underlying lib requires a keyword
+	d.Handle(NewDefaultHotword(alexaKeywordFile), nil)
+
+	handled := false
+	d.HandleSilenceFunc(1 * time.Second, func(keyword string) {
+		handled = true
+	})
+
+	silence, err := os.Open("audio/silence.wav")
+	if err != nil {
+		t.Error("can't open silence wav")
+	}
+	d.ReadAndDetect(silence)
+	if !handled {
+		t.Error("handler in HandleSilenceFunc never called")
+	}
 }
